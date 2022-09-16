112652ea9b310096f46d4494fb942b1affc0f6f1
jaxdcon <110577311+jaxdcon@users.noreply.github.com>
2022-09-16 11:10:28 -0400

    hello world

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("\n\nJaspal Marok | 000381777");
+  Serial.println(ESP.getChipId());
+  Serial.println(ESP.getFlashChipId());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  long myTime = millis();
+  Serial.println(myTime);
+
+  delay(2000);
 }
